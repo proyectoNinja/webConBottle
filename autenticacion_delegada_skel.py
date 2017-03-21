bfec388880f19dfdd97713400f1da72c208109fd
tfg <tfg@debian>
2017-03-21 19:34:26 +0100

    Code cleaning

--- autenticacion_delegada_skel.py
+++ autenticacion_delegada_skel.py
@@ -23,7 +23,7 @@ import urllib2, urllib
 import hashlib
 import os
 import json
-# Credenciales. 
+# Credenciales.
 # https://developers.google.com/identity/protocols/OpenIDConnect#appsetup
 # Copiar los valores adecuados.
 CLIENT_ID     = "64556356867-83l64d75qv4egbh0lh5go1nslatldu3p.apps.googleusercontent.com"
@@ -31,7 +31,7 @@ CLIENT_SECRET = "jZ6AMAO7TltKpSUZmXDWxgd6"
 REDIRECT_URI  = "http://localhost:8080/token"
 
 
-# Fichero de descubrimiento para obtener el 'authorization endpoint' y el 
+# Fichero de descubrimiento para obtener el 'authorization endpoint' y el
 # 'token endpoint'
 # https://developers.google.com/identity/protocols/OpenIDConnect#authenticatingtheuser
 DISCOVERY_DOC = "https://accounts.google.com/.well-known/openid-configuration"
@@ -43,9 +43,11 @@ TOKEN_VALIDATION_ENDPOINT = "https://www.googleapis.com/oauth2/v4/token"
 
 STATE =""
 @post('/uploader')
-def uploader():
+def uploader(var):
     upload = request.files.get('uploadedfile')
-    upload.save(upload.filename)
+    if(not os.direxists("../archivos/"+var)):
+        os.mkdir("../archivos/"+var)
+    upload.save("../archivos/"+var+"/"+upload.filename)
     return "<b>"+upload.filename+" </b>"
 
 @route('/')
@@ -53,22 +55,23 @@ def login_google():
     state = hashlib.sha256(os.urandom(1024)).hexdigest()
     response.set_cookie('state', state)
     p = "https://accounts.google.com/o/oauth2/v2/auth?client_id="+CLIENT_ID+"&response_type=code&scope=openid%20email&redirect_uri="+REDIRECT_URI+"&state="+state
-    return "<a href="+ p +" ><button>Pulse aquí para autenticar con google </button></a>" 
+    return "<a href="+ p +" ><button>Pulse aquí para autenticar con google </button></a>"
 
 @get('/token')
 def token():
     if request.cookies.get('state') == request.params.get('state'):
         code = request.params.get('code')
-        params = {"code":code,"client_id":CLIENT_ID,"client_secret":CLIENT_SECRET,"grant_type":"authorization_code","redirect_uri":REDIRECT_URI} 
+        params = {"code":code,"client_id":CLIENT_ID,"client_secret":CLIENT_SECRET,"grant_type":"authorization_code","redirect_uri":REDIRECT_URI}
         params = urllib.urlencode(params)
         m = urllib2.urlopen(TOKEN_VALIDATION_ENDPOINT, params)
         m = json.loads(m.read())
         p = urllib2.urlopen("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token="+m['id_token'])
         p = json.loads(p.read())
-        return "<b>Bienvenido "+p['email']+'''</b>  <form enctype="multipart/form-data" action="uploader" method="POST">
-<input name="uploadedfile" type="file" />
-<input type="submit" value="Subir archivo" />
-</form> '''
+        return "<b>Bienvenido "+p['email']+'''</b>
+                <form enctype="multipart/form-data" action="uploader?var=+p['email'] " method="POST">
+                    <input name="uploadedfile" type="file" />
+                    <input type="submit" value="Subir archivo" />
+                </form> '''
 #quitar HTML con template
     else:
         return "<b>ERROR</b>"
