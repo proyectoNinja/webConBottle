7e2543b9b06e09efc31a3bb3f06fc35b600497f6
usuario <usuario@debian>
2017-04-10 22:52:18 +0200

    space bug fixed 2

--- autenticacion_delegada_skel.py
+++ autenticacion_delegada_skel.py
@@ -6,7 +6,7 @@ import hashlib
 import os
 import json
 
-CLIENT_ID     = "1022856304800-ph8iuqc3rra0s7iac6ln81m083dcuvdv.apps.googleusercontent.com "
+CLIENT_ID     = "1022856304800-ph8iuqc3rra0s7iac6ln81m083dcuvdv.apps.googleusercontent.com"
 CLIENT_SECRET = "P1juZRvd3EkQUz-hX9nkAJa6"
 REDIRECT_URI  = "http://mifulo.dacya.ucm.es:8080/token"
 DISCOVERY_DOC = "https://accounts.google.com/.well-known/openid-configuration"
